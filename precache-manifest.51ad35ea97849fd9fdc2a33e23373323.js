a4627978be26ea87c4dcf6ee3704b5867f851e5b
chenl101 <chenl15213@gmail.com>
2020-01-20 19:30:05 -0500

    Updates

---
+++ precache-manifest.51ad35ea97849fd9fdc2a33e23373323.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857fb3de94537c6080e3be49f6336c2a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3995d7f66b58312b075e",
+    "url": "/monsters-rolodex/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "5ad9e9d6fec0ec135cb7",
+    "url": "/monsters-rolodex/static/js/2.f6711c6e.chunk.js"
+  },
+  {
+    "revision": "3995d7f66b58312b075e",
+    "url": "/monsters-rolodex/static/js/main.d2bbb488.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
